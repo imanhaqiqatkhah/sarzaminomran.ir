6494af24a2f9d1297f66b6e7bb15f30fd6cc99f1
sarzaminomran <imanhaqiqatkhah@gmail.com>
2024-09-18 15:28:15 +0330

    edited address images

--- components/Bulb.js
+++ components/Bulb.js
@@ -5,7 +5,7 @@ const Bulb = () => {
   return (
     <div className="absolute xl:-left-28 -left-16 bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[140px] xl:w-[200px]">
       <Image
-        src={"./bulb.png"}
+        src={"../public/bulb.png"}
         width={260}
         height={200}
         alt=""
